184ffabe12f4a9cf6b2f6ab67a7bd3c041a67e84
Blair Englund <blair.englund@gmail.com>
2016-08-01 12:07:41 -0500

    added event listeners to open/close form

--- scripts.js
+++ scripts.js
@@ -1,11 +1,30 @@
 window.addEventListener('load', function() {
-	
+
+	//button to open up form
+	var addAlbum = document.getElementById('addAlbum');
+
+	//header which expands when clicked
+	var headerform = document.getElementById('headerform');
+
+	//form that appears
+	var musicform = document.getElementById('musicform');
+
+	//close form button
+	var close = document.getElementById('close');
+
+	addAlbum.addEventListener("click", function() {
+		headerform.style.height = "600px";
+		musicform.style.display = "flex";
+		close.style.display = "block";
+	});
+
+	close.addEventListener("click", function() {
+		headerform.style.height = "90px";
+		musicform.style.display = "none";
+		close.style.display = "none";
+	})
 
 
 
 
-
-
-
-	
 })
