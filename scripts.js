e42486abb9c2971227513e17d871bc6164f3be31
Blair Englund <blair.englund@gmail.com>
2016-08-01 14:59:39 -0500

    working on sending and receiving music info

--- scripts.js
+++ scripts.js
@@ -1,28 +1,60 @@
 window.addEventListener('load', function() {
 
+	//CLICKABLE ELEMENTS -------------------------------------------------------------
+
 	//button to open up form
 	var addAlbum = document.getElementById('addAlbum');
-
-	//header which expands when clicked
-	var headerform = document.getElementById('headerform');
-
-	//form that appears
-	var musicform = document.getElementById('musicform');
 
 	//close form button
 	var close = document.getElementById('close');
 
+	//next page button
+	var next = document.getElementById('nextbutton')
+
+
+	//CHANGING ELEMENTS --------------------------------------------------------------
+
+	//header (expands when "Add an Album is clicked)
+	var headerform = document.getElementById('headerform');
+
+	//music form (appears when "Add an Album" is clicked)
+	var musicform = document.getElementById('musicform');
+
+
+
+	//DOM ----------------------------------------------------------------------------
+
+	//Expand header and open form
 	addAlbum.addEventListener("click", function() {
 		headerform.style.height = "600px";
-		musicform.style.display = "flex";
+		musicform.style.display = "block";
 		close.style.display = "block";
 	});
 
+	//Close form and shrink header
 	close.addEventListener("click", function() {
 		headerform.style.height = "90px";
 		musicform.style.display = "none";
 		close.style.display = "none";
+		musicform.reset();
 	})
+
+
+
+	//FORM INPUT AND SUBMISSION ----------------------------------------------------
+
+	//When next is clicked on first part, see if the album already exists
+	next.addEventListener('click', function() {
+		var request = new XMLHttpRequest();
+
+		var musicformdata = new DataForm(musicform);
+
+		request.open
+
+
+	})
+
+
 
 
 
